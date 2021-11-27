74ba7d34cc709e11c0acf31a4fca9e7a378b4e70
Anmol Gupta <guptaanmol184@gmail.com>
2021-11-27 09:53:06 +0530

    Completes exercises 3.1 - 3.8 (phonebook)
    - Create node js backend
    - Use nodemon for watching edits, and reloading the backend server
    - Use express for creating http server
    - Create backend CRUD APIs (/api/persons) for editing phonebook in the backend (GET, PUT, DELETE, POST) with error handling
    - Use vscode REST Client to define request/responses for testing in vscode
    - Use morgan middleware for logging defining a new token for printing the request body data in a POST request

---
+++ part-03/phonebook/index.js
@@ -0,0 +1,130 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+app.use(express.json())
+
+morgan.token('post-body', (request, response) => {
+  return request.method === 'POST'
+    ? JSON.stringify(request.body)
+    : null
+})
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :post-body')
+)
+
+const BASE_URL = '/api/persons'
+
+let persons = [
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+
+// Root api
+app.get('/', (request, response) => {
+  response.send('<h2>Welcome to Persons api</h2>')
+})
+
+// Info api
+app.get(`${BASE_URL}/info`, (request, response) => {
+  bodyString =
+    [`<p>Phonebook has info for ${persons.length} people.</p>`,
+    `<p>Server Time: ${Date()}</p>`]
+      .join('\n')
+
+  response.send(bodyString)
+})
+
+// List all persons
+app.get(`${BASE_URL}`, (request, response) => {
+  response.json(persons)
+})
+
+// Query person by id
+app.get(`${BASE_URL}/:id`, (request, response) => {
+  const personId = Number(request.params.id)
+
+  foundPerson = persons.find(p => p.id === personId)
+
+  if (foundPerson) {
+    response.json(foundPerson)
+  } else {
+    response.status(404).end()
+  }
+})
+
+// Delete person by id
+app.delete(`${BASE_URL}/:id`, (request, response) => {
+  const personId = Number(request.params.id)
+
+  persons = persons.filter(p => p.id != personId)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  return Math.floor(Math.random() * 100)
+}
+
+// Add new person
+app.post(`${BASE_URL}`, (request, response) => {
+
+  let newPerson = request.body
+
+  if (!newPerson.name) {
+    return response
+      .status(400)
+      .json({
+        error: 'name missing'
+      })
+  }
+
+  if (!newPerson.number) {
+    return response
+      .status(400)
+      .json({
+        error: 'number missing'
+      })
+  }
+
+  if (persons.findIndex(p => p.name === newPerson.name) !== -1) {
+    return response
+      .status(400)
+      .json({
+        error: 'name must be unique'
+      })
+  }
+
+  let newPersonId = -1
+  do {
+    newPersonId = generateId()
+  } while (persons.findIndex(p => p.id == newPersonId) !== -1)
+
+  newPerson.id = newPersonId
+  persons = persons.concat(newPerson)
+
+  response.json(newPerson)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Express server running on port ${PORT}`)
+})
