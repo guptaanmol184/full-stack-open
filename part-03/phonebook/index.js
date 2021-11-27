13eb3d46e182e0524a054bbb7fe013d41bc714b1
Anmol Gupta <guptaanmol184@gmail.com>
2021-11-27 18:23:42 +0530

    Completes exercises 3.15 - 3.18 (phonebook)
    - Update delete API to delete entries in mongoDB
    - Add a error handling and unknow endpoint middleware
    - Add PUT API to update existing person using the person's id
    - Updated INFO api to make use of the entries in mongoDB

--- part-03/phonebook/index.js
+++ part-03/phonebook/index.js
@@ -24,40 +24,54 @@ app.use(
 const BASE_URL = '/api/persons'
 
 // Info api
-app.get(`${BASE_URL}/info`, (request, response) => {
-  bodyString =
-    [`<p>Phonebook has info for ${persons.length} people.</p>`,
-    `<p>Server Time: ${Date()}</p>`]
-      .join('\n')
+app.get(`${BASE_URL}/info`, (request, response, next) => {
 
-  response.send(bodyString)
+  Person
+  .find({})
+  .then(persons => {
+    let personCount = persons.length
+    bodyString =
+      [`<p>Phonebook has info for ${personCount} people.</p>`,
+      `<p>Server Time: ${Date()}</p>`]
+        .join('\n')
+    response.send(bodyString)
+  })
+  .catch(error => next(error))
 })
 
 // List all persons
-app.get(`${BASE_URL}`, (request, response) => {
-  Person.find({}).then(persons => {
+app.get(`${BASE_URL}`, (request, response, next) => {
+  Person
+  .find({})
+  .then(persons => {
     response.json(persons)
   })
+  .catch(error => next(error))
 })
 
 // Query person by id
-app.get(`${BASE_URL}/:id`, (request, response) => {
+app.get(`${BASE_URL}/:id`, (request, response, next) => {
   Person.findById(request.params.id).then(person => {
-    response.json(person)
+    if(person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
 
 // Delete person by id
-app.delete(`${BASE_URL}/:id`, (request, response) => {
-  const personId = Number(request.params.id)
-
-  persons = persons.filter(p => p.id != personId)
-
-  response.status(204).end()
+app.delete(`${BASE_URL}/:id`, (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+    })
+  .catch(error => next(error))
 })
 
 // Add new person
-app.post(`${BASE_URL}`, (request, response) => {
+app.post(`${BASE_URL}`, (request, response, next) => {
 
   let newPerson = request.body
 
@@ -77,22 +91,67 @@ app.post(`${BASE_URL}`, (request, response) => {
       })
   }
 
-// TODO: Ensuring no duplicate names are added to the db
-//  if (persons.findIndex(p => p.name === newPerson.name) !== -1) {
-//    return response
-//      .status(400)
-//      .json({
-//        error: 'name must be unique'
-//      })
-//  }
-
   const person = new Person({
     number: newPerson.number,
     name: newPerson.name
   }) 
   person.save()
   .then(savedPerson => response.json(savedPerson))
+  .catch(error => next(error))
+
+
 })
+
+// Update existing person
+app.put(`${BASE_URL}/:id`, (request, response, next) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response
+      .status(400)
+      .json({
+        error: 'name missing'
+      })
+  }
+
+  if (!body.number) {
+    return response
+      .status(400)
+      .json({
+        error: 'number missing'
+      })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+// handler of requests with unknown endpoint
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+
+// handlers for request with result to errors
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if(error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
