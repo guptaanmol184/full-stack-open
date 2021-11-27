6257a1a02b599e51a239e925813035f4da304ba7
Anmol Gupta <guptaanmol184@gmail.com>
2021-11-27 22:04:03 +0530

    Completes exercises 4.1 - 4.2 (bloglist)
    - Creates an npm express project for the blog list application backend
    - Improves development using nodemon
    - Refactors the project in modules

---
+++ part-04/bloglist/utils/config.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+require('dotenv').config()
+
+const PORT = process.env.PORT
+const MONGODB_URL = process.env.MONGODB_URL
+
+module.exports = {
+  PORT,
+  MONGODB_URL
+}
