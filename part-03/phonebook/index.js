7f10d25139230ab5060436f42345565c534c880e
Anmol Gupta <guptaanmol184@gmail.com>
2021-11-27 13:19:40 +0530

    Completes exercises 3.9 (phonebook)
    - Adds cors middleware to express for allowing Cross Origin requests

--- part-03/phonebook/index.js
+++ part-03/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 morgan.token('post-body', (request, response) => {
